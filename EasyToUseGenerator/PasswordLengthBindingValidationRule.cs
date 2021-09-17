a7bf739c99436bfb67c8efa7170fa22c36b25412
Benjamin Ellett <30413162+BenjaminEllett@users.noreply.github.com>
2021-09-16 17:58:19 -0700

    The Create New Password dialog box now validates a new password's length (#20)
    The Create New Password dialog box now validates a new password's length.  
    
    Closes #20

---
+++ EasyToUseGenerator/PasswordLengthBindingValidationRule.cs
@@ -0,0 +1,81 @@
+﻿//
+// MIT License
+//
+// Copyright(c) 2019-2021 Benjamin Ellett
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using CommonGeneratorCode;
+using EasyToUseGenerator.Resources;
+using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace EasyToUseGenerator
+{
+    public class PasswordLengthBindingValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (typeof(string) != value.GetType())
+            {
+                throw new ArgumentException("This class can only validate strings.", paramName: nameof(value));
+            }
+
+            int potentialNewLength;
+
+            if (!int.TryParse(
+                    (string)value, 
+                    NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite, 
+                    cultureInfo, 
+                    out potentialNewLength))
+            {
+                return CreateInvalidPasswordLengthResult();
+            }
+
+            if (!Password.IsValidPasswordLength(potentialNewLength))
+            {
+                return CreateInvalidPasswordLengthResult();
+            }
+
+            // This application only supports passwords which have between 1 and 38 characters.  The reason
+            // is the application's UI cannot display passwords longer than 38 charactes and there is no 
+            // good reason to support longer passwords.  A non-numeric password which is 22 characters long
+            // has more than 2^128 possible values.  It is unfeasible to guess these passwords with current
+            // technology.  There is no benefit to having a 39 or more character password because a 38
+            // character password is already unguessable.
+            const int MaxSupportedPasswordLengthInThisApplication = 38;
+
+            if (potentialNewLength > MaxSupportedPasswordLengthInThisApplication)
+            {
+                return CreateInvalidPasswordLengthResult();
+            }
+
+            return ValidationResult.ValidResult;
+
+
+
+            ValidationResult CreateInvalidPasswordLengthResult()
+            {
+                return new ValidationResult(isValid: false, errorContent: ErrorMessages.InvalidNewPasswordLength);
+            }
+        }
+    }
+}
---
+++ EasyToUseGeneratorTests/PasswordLengthBindingValidationRuleTests.cs
@@ -0,0 +1,100 @@
+﻿using EasyToUseGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using TestUtil;
+
+namespace EasyToUseGenerator.Tests
+{
+    [TestClass()]
+    public class PasswordLengthBindingValidationRuleTests
+    {
+        // The lengths were chosen because they are common password lengths.
+        [DataTestMethod()]
+        [DataRow("1")] // Minimum password length
+        [DataRow("4")]
+        [DataRow(" 4")] // A password with a leading white space character
+        [DataRow("6")]
+        [DataRow("6 ")] // A password with a trailing white space character
+        [DataRow("8")]
+        [DataRow(" 8 ")] // A password with a leading and trailing white space character
+        [DataRow("8")]
+        [DataRow("10")]
+        [DataRow("22")]
+        [DataRow("   22")] // A password with multiple leading white space characters
+        [DataRow("22  ")] // A password with multiple trailing white space characters
+        [DataRow("        22      ")] // A password with multiple leading and trailing white space characters
+        [DataRow("24")]
+        [DataRow("38")] // Largest supported password length
+
+        public void ValidateShould_Accept_ValidPasswordLengths(string validPasswordLength)
+        {
+            ValidationResult result = ValidatePasswordLength(validPasswordLength);
+            Assert.IsTrue(result.IsValid, "The result should be valid because a valid input was passed to the Validate() method");
+            Assert.IsNull(result.ErrorContent, "ErrorContent should be null because IsValid() returned true");
+        }
+
+        [DataTestMethod()]
+
+        // The empty string is an invalid length because it contains no characters.  
+        [DataRow("")]
+
+        // This string is an invalid length because it is not a number.
+        [DataRow("X")]
+
+        // This string is an invalid length because it is not a number.
+        [DataRow("Abcdefg")]
+
+        // -1 is an invalid length because a password cannot have a negative number of characters.
+        [DataRow("-1")]
+
+        // 0 is an invalid password length because a password must have at least 1 character (a 1 character password is 
+        // insecure but is it still a valid password).
+        [DataRow("0")]
+
+        // Passwords cannot have a fractional number of characters (what does it even mean to have a password with 10.4 characters in it?).
+        [DataRow("10.4")]
+
+        // The GUI version of the application supports at most 38 characters in a password.
+        [DataRow("39")]
+
+        // Passwords can have at most 256 characters.
+        [DataRow("257")]
+
+        // Test the code which hands integer overflows and underflows.  In C#, an int's value can go from -2,147,483,648 to 2,147,483,647 .
+        // (https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/int)
+        [DataRow("-2147483649")]
+        [DataRow("2147483648")]
+        public void ValidateShould_Reject_InvalidPasswordLengths(string invalidPasswordLength)
+        {
+            ValidationResult result = ValidatePasswordLength(invalidPasswordLength);
+            Assert.IsFalse(result.IsValid, "The result should be invalid because a valid password length was NOT passed to the Validate() method");
+            Assert.IsNotNull(result.ErrorContent, "ErrorContent should NOT be null because IsValid() returned false");
+            Assert.IsNotNull(result.ErrorContent.GetType() == typeof(string), "ErrorContent should contain a string which can be displayed to the user.");
+
+            string errorMessage = (string)result.ErrorContent;
+            Assert.IsFalse(string.IsNullOrWhiteSpace(errorMessage), "The error message should contain a string which tells the user how to correct the error.");
+        }
+
+        [TestMethod]
+        public void ValidShouldThrowAnExceptionIfAnUnsupportedTypeIsPassedToIt()
+        {
+            byte unsupportedType = 1;
+
+            TestHelper.TestActionWhichShouldThrowAnException<ArgumentException>(
+                () => ValidatePasswordLength(unsupportedType),
+                expectedExceptionText: "This class can only validate strings.");
+        }
+
+        private static ValidationResult ValidatePasswordLength<T>(T potentialPasswordLength)
+        {
+            var passwordLengthBindingValidationRule = new PasswordLengthBindingValidationRule();
+            return passwordLengthBindingValidationRule.Validate(potentialPasswordLength, CultureInfo.InvariantCulture);
+        }
+    }
+}
